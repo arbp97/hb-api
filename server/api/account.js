913b2a946b6758ea4ca8156d27642368758cf966
arbp97 <arbp97@outlook.com>
2022-03-15 15:58:00 -0300

    <fix>: fix bug related to issue #9

--- server/api/account.js
+++ server/api/account.js
@@ -7,34 +7,31 @@ const router = Router();
     should receive this json object:
     {
         email : ...
-        pass : ...
+        password : ...
     }
 */
-router.post("/account/validate", (req, res) => {
+router.post("/account/validate", async (req, res) => {
   let data = req.body;
-  //console.log("BODY" + data.email);
-  Account.findByMail(data.email)
-    .then((account) => {
-      //console.log("ACC" + account);
 
-      if (account) {
-        account.comparePassword(data.password, function (err, isMatch) {
-          if (err) throw err;
+  try {
+    let account = await Account.findByMail(data.email);
 
-          if (isMatch) {
-            res.status(200).json({ msg: "welcome back" });
-          } else {
-            res.status(400).json({ msg: "incorrect password" });
-          }
-        });
-      } else {
-        res.status(404).json({ msg: "email not found" });
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).json({ msg: error });
-    });
+    if (account) {
+      account.comparePassword(data.password, function (err, isMatch) {
+        if (err) throw err;
+
+        if (isMatch) {
+          res.status(200).json({ msg: "ok" });
+        } else {
+          res.status(400).json({ msg: "incorrect_password" });
+        }
+      });
+    } else {
+      res.status(404).json({ msg: "not_found" });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 });
 
 module.exports = router;
