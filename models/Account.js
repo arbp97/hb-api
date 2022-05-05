aff6d849af97344394126631e5c674e3b1433d18
arbp97 <arbp97@outlook.com>
2022-05-05 18:19:14 -0300

    test: refactor to deploy, episode 10: replace bcrypt, add bcryptjs

--- models/Account.js
+++ models/Account.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const Transaction = require("../models/Transaction");
 const Currency = require("../models/Currency");
 
