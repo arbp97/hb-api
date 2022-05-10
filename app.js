dcda3fa2bccf8b58cf6f17192ff5ecf6cc9e0738
arbp97 <arbp97@outlook.com>
2022-05-09 23:14:22 -0300

    refactor: move test data & uploading functions to another module, add some style to index page

--- app.js
+++ app.js
@@ -4,16 +4,7 @@ import { connectDatabase } from "./database.js";
 import bodyParser from "body-parser";
 const { urlencoded, json } = bodyParser;
 import cors from "cors";
-// === Models ===
-import { UserModel } from "./models/User.js";
-import { AccountModel } from "./models/Account.js";
 import { updateCurrencies } from "./models/Currency.js";
-import { TransactionModel } from "./models/Transaction.js";
-// === Test Data ===
-//import * as Users from "./data/users.json" assert { type: "json" };
-//import * as Accounts from "./data/accounts.json" assert { type: "json" };
-//import * as Transactions from "./data/transactions.json" assert { type: "json" };
-
 import routes from "./routes.js";
 
 const app = express();
@@ -35,49 +26,13 @@ app.use(json());
 
 //api routes
 app.use("/", routes);
+app.use(express.static("public/"));
 
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
-// loading data in DB
-
-const saveAll = async (loadFromJson) => {
-  const currencyStatus = await updateCurrencies();
-  console.log(currencyStatus);
-  /*
-  if (loadFromJson) {
-    for (const U of Users) {
-      try {
-        await new UserModel(U).save();
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    for (const A of Accounts) {
-      try {
-        await new AccountModel(A).save();
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    for (const T of Transactions) {
-      try {
-        await new TransactionModel(T).save();
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    console.log("json data uploaded");
-  }*/
-};
-
 // get updated currency rates info every 15min
-setInterval(saveAll, 900000, false);
-
-//saveAll(false);
+setInterval(updateCurrencies, 900000);
 
 app.listen(port, console.log(`Server started on port ${port}`));
 
